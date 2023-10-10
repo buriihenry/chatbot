83b6270d4abb6d2ffd5873c37b7c3405091bb8d6
buriihenry <buriihenry@gmail.com>
2023-10-10 04:48:52 +0300

    pycode

--- chatbot.py
+++ chatbot.py
@@ -8,3 +8,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import warnings
 warnings.filterwarnings('ignore')
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True)
+
+# uncomment the following only the first time
+nltk.download('punkt') # first-time use only
+nltk.download('wordnet') # first-time use only
