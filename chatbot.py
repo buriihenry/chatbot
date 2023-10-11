2a35e93dbaf1409e81e4d917f21ff49be11abe40
buriihenry <buriihenry@gmail.com>
2023-10-11 05:00:59 +0300

    added code

--- chatbot.py
+++ chatbot.py
@@ -18,5 +18,19 @@ nltk.download('punkt') # first-time use only
 nltk.download('wordnet') # first-time use only
 
 # Reading in the corpus
-with open('chatbot.text', 'r', encoding='utf8', errors='ignore') as fin:
+with open('chatbot.txt', 'r', encoding='utf8', errors='ignore') as fin:
     raw = fin.read().lower()
+
+#Tokenization
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw)
+
+#Preprocessing
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
