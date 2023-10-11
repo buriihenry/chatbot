dbd14d748c11a78ed1f4c28463df03dd32945180
buriihenry <buriihenry@gmail.com>
2023-10-11 04:45:44 +0300

    Added python code

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+#import necessary libraries
+import io
+import random
+import string # to process standard python strings
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
